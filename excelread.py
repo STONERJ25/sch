c2191a9e971189f15538f453b354a694ae9cf4ac
STONERJ25 <stonerj@etown.edu>
2023-11-18 04:16:46 -0500

    Comments are for the weak

--- excelread.py
+++ excelread.py
@@ -2,68 +2,64 @@ import pandas as pd
 import sys
 df = pd.read_excel(r'Book.xlsx')
 def get_days(code):
-    column_name4 = 'Code'  # Replace with the name of your column
+    column_name4 = 'Code'  
     df[column_name4].fillna('', inplace=True)
     result4 = df[df[column_name4].str.contains(code)]
     matching_row_numbers4 = result4.index.tolist()
     Day_for_course = []
     for i in range(len(matching_row_numbers4)):
-        cell4 = df.at[matching_row_numbers4[i], 'Day']  # Note that Excel uses 1-based indexing for rows, 0-based for columns
+        cell4 = df.at[matching_row_numbers4[i], 'Day']  
         if not pd.isna(cell4):
             
             Day_for_course.append(cell4)
     return Day_for_course
 
 def get_time_slots(code):
-    column_name = 'Code'  # Replace with the name of your column
+    column_name = 'Code' 
     df[column_name].fillna('', inplace=True)
     result = df[df[column_name].str.contains(code)]
     matching_row_numbers = result.index.tolist()
     time_list_for_course = []
     for i in range(len(matching_row_numbers)):
-        cell = df.at[matching_row_numbers[i], 'Time']  # Note that Excel uses 1-based indexing for rows, 0-based for columns
+        cell = df.at[matching_row_numbers[i], 'Time']  
         if not pd.isna(cell):
             
             time_list_for_course.append(cell)
     return time_list_for_course
 def get_credit_amount(code):
-    column_name2 = 'Code'  # Replace with the name of your column
+    column_name2 = 'Code' 
     df[column_name2].fillna('', inplace=True)
     result3 = df[df[column_name2].str.contains(code)]
     matching_row_numbers1 = result3.index.tolist()
     credit = []
     for i in range(len(matching_row_numbers1)):
-        cell1 = df.at[matching_row_numbers1[i], 'Credits']  # Note that Excel uses 1-based indexing for rows, 0-based for columns
+        cell1 = df.at[matching_row_numbers1[i], 'Credits'] 
         if not pd.isna(cell1):
             if cell1 not in credit:
                 credit.append(cell1)
     return credit[0]
 def course_name_to_code(name):
-    column_name4 = 'Course Title'  # Replace with the name of your column
+    column_name4 = 'Course Title' 
     df[column_name4].fillna('', inplace=True)
     result4 = df[df[column_name4].str.contains(name)]
     matching_row_numbers4 = result4.index.tolist()
     Code_for_course = []
     for i in range(len(matching_row_numbers4)):
-        cell4 = df.at[matching_row_numbers4[i], 'Code List']  # Note that Excel uses 1-based indexing for rows, 0-based for columns
+        cell4 = df.at[matching_row_numbers4[i], 'Code List'] 
         if not pd.isna(cell4):
             
             Code_for_course.append(cell4)
     return Code_for_course[0]
 class Course:
     def __init__(self, code):
-        # Instance attributes (unique to each instance)
+        
         if len(code) > 6:
             self.code = course_name_to_code(code)
         else:
             self.code = code
         print(self.code)
         self.time_slots = get_time_slots(self.code)
-        #self.time_slots_mins = time_string_to_minutes(self.time_slots)
+       
         self.credits = get_credit_amount(self.code)
         self.days = get_days(self.code)
 
-#course1 = Course('EGR192')
-#print(course1.time_slots)
-#print(course1.credits)
-#print(course1.days)
--- main.py
+++ main.py
@@ -10,23 +10,23 @@ instances = []
 column_list1 = df['Code List'].dropna().tolist()
 column_list2 = df['Course Title'].dropna().tolist()
 def time_string_to_minutes(time_str):
-    # Split the time string into start and end times
+
     times1111 = time_str.split('-')
     start_time =times1111[0]
     end_time =times1111[1]
-    # Function to convert HH:MM[am/pm] to minutes since midnight
+
     def time_to_minutes(time):
-      #  print(time)
+
         hh, mm, ampm = time[:2], time[-4:-2], time[-2:]
 
         hh = int(hh)
         mm = int(mm)
         if ampm == 'pm' and hh != 12:
             hh += 12
-       # print(hh * 60 + mm)
+
         return hh * 60 + mm
 
-    # Convert start and end times to minutes
+
     start_minutes = time_to_minutes(start_time)
     end_minutes = time_to_minutes(end_time)
 
@@ -47,19 +47,19 @@ def conflict(dt1,dt2):
     time1 = dts1[0]
     days2 = dts2[1]
     time2 = dts2[0]
-   # print(time1)
+
     time11 = string_to_tuple(time_string_to_minutes(time1))
     time22 = string_to_tuple(time_string_to_minutes(time2))
     if have_common_letter(days1, days2):
         if have_common_time(time11, time22):
-           # print("We have a common time")
+
             return True
         else:
-            #print("Pass conflict")
+
             return False
             
     else:
-        #print("Pass days")
+
         return False
 
 def have_common_time(time1, time2):
@@ -68,36 +68,35 @@ def have_common_time(time1, time2):
 
 
 def have_common_letter(string1, string2):
-    # Create sets of characters for both strings
+
     set1 = set(string1)
     set2 = set(string2)
 
-# Combine the two lists
+
 
 def on_search(entry_text):
-    # Function to handle the search logic
-    # In this example, it simply filters a list based on the input
+
+
     filtered_results = [item for item in all_results if entry_text.lower() in item.lower()]
 
-    # Update the Listbox with the filtered results
-    result_listbox.delete(0, tk.END)  # Clear the current items
+    result_listbox.delete(0, tk.END)  
     for result in filtered_results:
         result_listbox.insert(tk.END, result)
 
 def add_to_selected_list():
-    # Function to add the selected item to the another list
+
     selected_item = result_listbox.get(tk.ACTIVE)
     if selected_item:
         selected_listbox.insert(tk.END, selected_item)
 
 def remove_selected_item():
-    # Function to remove the selected item from the second list
+
     selected_index = selected_listbox.curselection()
     if selected_index:
         selected_listbox.delete(selected_index)
 
 def out_put_list():
-    # Function to remove the selected item from the second list
+
     instances = []
     selected_items = selected_listbox.get(0, tk.END)
     print("Selected Items:")
@@ -105,12 +104,12 @@ def out_put_list():
         print(item)
         instance = Course(item)
     
-    # Add the instance to the list
+
         instances.append(instance)
     mainlogi(instances)
 def mainlogi(list_course):
     total_credits=0
-    # Print the instances
+
     for i, instance in enumerate(list_course):
         print(f"Course {i + 1}: Name: {instance.code} Time Slots: {instance.time_slots} Credits: {instance.credits} Days: {instance.days}")
     for i, instance in enumerate(instances):
@@ -126,24 +125,22 @@ def mainlogi(list_course):
 
     master_list = []
 
-    # Initialize master_list with empty lists
-    # Determine the dimensions of the 2D list
+
     num_instances = len(list_course)
     max_time_slots = max(len(instance.time_slots) for instance in list_course)
 
-    # Initialize master_list as a 2D list
+
     master_list = [[0] * max_time_slots for _ in range(num_instances)]
 
     for i, instance in enumerate(list_course):
         for j in range(len(instance.time_slots)):
-            master_list[i][j] += 1  # Increment the value at index [i][j]
+            master_list[i][j] += 1  
 
 
 
-    # Count the number of 1s in each sub-list and store the counts in a new list
+
     count_of_ones = [sum(1 for element in sublist if element == 1) for sublist in master_list]
-    # Print the counts
-    #print(count_of_ones)
+
     course_lister = []
     for r in range(len(count_of_ones)):
         temp_course = []
@@ -151,9 +148,9 @@ def mainlogi(list_course):
             
             temp_course.append(f'{list_course[r].time_slots[q]}'+' '+f'{list_course[r].days[q]}')
         course_lister.append(temp_course)
-    #print(course_lister)
+
     from itertools import product
-    #print(course_lister)
+
     def all_combinations(lst):
         if not lst:
             return [()]
@@ -161,17 +158,17 @@ def mainlogi(list_course):
         first_list = lst[0]
         rest_lists = lst[1:]
         
-        # Recursively get all combinations of the rest of the lists
+
         rest_combinations = all_combinations(rest_lists)
         
-        # For each element in the first list, combine it with all combinations of the rest
+    
         result = []
         for element in first_list:
             result.extend((element, *comb) for comb in rest_combinations)
         
         return result
 
-    # Example usage:
+
 
     combinations = all_combinations(course_lister)
 
@@ -184,7 +181,7 @@ def mainlogi(list_course):
         return False
     flag = True
     for combo in combinations:
-        #print(list(combo))
+
         result = has_shared_property(list(combo), conflict)
         if result==False:
             print("Here is a Schudule that works:" f'{combo}')
@@ -198,51 +195,46 @@ def mainlogi(list_course):
             pass
             #print('nope')
     if  flag:
-        print("No possible Schdule found")
+        print("No possible Schedule found")
 
 l1 = list(set(column_list1))
 l2 = list(set(column_list2))
 l1.sort()
 l2.sort()
 
-    
-# Sample data for demonstration
+
 all_results = l1+l2
-#["Apple", "Banana", "Orange", "Grapes", "Watermelon", "Pineapple", "Mango", "Strawberry"]
 
-# Create the main window
+
+
 root = tk.Tk()
-root.title("Dynamic Search Example")
+root.title("Schedule Generator")
 
-# Create an Entry widget for search input
 search_entry = tk.Entry(root, width=30)
 search_entry.pack(pady=10)
-
-# Create a Listbox to display search results
 result_listbox = tk.Listbox(root, selectmode=tk.SINGLE, width=30)
 result_listbox.pack()
 
-# Create a Listbox to display selected items
+
 selected_listbox = tk.Listbox(root, selectmode=tk.SINGLE, width=30)
 selected_listbox.pack(pady=10)
 
-# Bind the search function to the Entry widget
+
 search_entry.bind("<KeyRelease>", lambda event: on_search(search_entry.get()))
 
-# Bind the add_to_selected_list function to the Enter key and Double-Click events
+
 result_listbox.bind("<Return>", lambda event: add_to_selected_list())
 result_listbox.bind("<Double-Button-1>", lambda event: add_to_selected_list())
 
-# Button to remove selected item from the second list
 remove_button = tk.Button(root, text="Remove Selected", command=remove_selected_item)
 remove_button.pack()
-# Button to remove selected item from the second list
+
 output_button = tk.Button(root, text="Generate Schedule", command=out_put_list)
 output_button.pack()
 
-# Initial display of all results
+
 for result in all_results:
     result_listbox.insert(tk.END, result)
 
-# Start the main event loop
+
 root.mainloop()
